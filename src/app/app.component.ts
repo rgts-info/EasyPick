664ad1b62b7b794d6e8df454b66263a2f93d73cb
rgts-info <hlklouie@gmail.com>
2026-07-07 19:13:07 +0800

    Update banner component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 // private hkjcsportServiceProvider : HkjcsportserviceProvider,
 @Component({
@@ -6,11 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  show_spinner: boolean = true;
+
   constructor() {
 
-    console.log("INSIDE appflyer app component");
+    console.log("INSIDE listing-easypick GitHub page app.components.ts");
     
 
   }
+
+  ngOnInit() {
+    console.log('Spinner state:', this.show_spinner);
+  }
+
+
 }
